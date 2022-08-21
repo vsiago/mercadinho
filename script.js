154e6466a850c7b8b6e780d0518b607bf3d0707b
vsiago <vsiago22@gmail.com>
2022-08-21 09:55:13 -0300

    script.js atualizado (primeiro programa válido)

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-Este é um teste
+let nome = prompt('Qual o seu nome?');
+let idade = prompt('Qual a sua idade?');
+
+
+function bemVindo(nome, idade){
+    console.log(`Bem-vindo, ${nome}. Você tem ${idade} anos, parabéns!`);
+};
+
+console.log(bemVindo(nome, idade));
